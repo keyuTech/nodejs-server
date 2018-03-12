f7f931f8efa2d31c4bb6daee9e40b559acf2a766
wky0615 <wky_0615@163.com>
2018-03-12 19:37:25 +0800

    实现JSONP

--- server.js
+++ server.js
@@ -10,68 +10,36 @@ if(!port){
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-
-
-
-
-
-
-
-
-
-
-
-  console.log('HTTP 路径为\n' + path)
-
-  switch (true){
-    case (path == '/'):
-      console.log('匹配/')
-      response.setHeader('Content-Type', 'text/html')
-      response.write('<!DOCTYPE>\n<html>'  + 
-        '<head>\n<link rel="stylesheet" href="/style.css">' +
-        '</head>\n<body>'  +
-        '<h1>Hello World</h1>' +
-        '<script src="/main.js"></script>' +
-        '</body>\n</html>')
-      response.end()
-      break
-    case (path == '/style.css'):
-      console.log('匹配/style.css')
-      response.setHeader('Content-Type', 'text/css')
-      response.write('h1{color: red;}')
-      response.end()
-      break
-    case (path == '/main.js'):
-      console.log('匹配/main.js')
-      response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-      response.write('alert("Hi")')
-      response.end()
-      break
-    default:
-      response.statusCode = 404
-      response.end()
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('哈哈哈')
+    response.end()
+  }else if(path === '/getmsg'){
+    response.statusCode = 200
+    response.setHeader('Content-type', 'application/javascript')
+    response.write(`${query.callback}.call(undefined, '得到jack.com:8002的信息')`)
+    response.end()
+  }else{
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
+    response.end()
   }
-
-
-
-
-
-
-
 
   /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
